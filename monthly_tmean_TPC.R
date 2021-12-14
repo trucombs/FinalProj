3c09dfea1f427c4e367ab9a30c42e663e12df0ee
trucombs <tpcombs@email.arizona.edu>
2021-12-14 07:17:58 +0000

    addt'l tweaks before sharing with group

--- monthly_tmean_TPC.R
+++ monthly_tmean_TPC.R
@@ -2,6 +2,7 @@ library(prism) #load prism library
 
 prism_set_dl_dir("~/FinalProj/prism_monthly") #set this directory to the location that you will save the prism data (create a new folder, if needed)
 
+#takes a few seconds to download from PRISM
 get_prism_monthlys(type = "tmean", year = 2020, mon = 1:12, keepZip = FALSE) #downloading prism data to directory above
 #get_prism_annual("ppt", years = 2000:2015, keepZip = FALSE)
 
@@ -17,11 +18,12 @@ len <- 103 # number of fires in the fire dataset
 list <- vector(mode = "list", length = len) #create an empty vector that will contain the prism curves for all sites
 list_site <- c() #storing 12 months for each fire in vector for future use
 
+#loops through all sites to extract site name and mean monthly temp -- takes a few minutes
 for (i in 1:dim(fire)[1]) {
   temp_list <- rep(toString(fire$INC_Name[i]), length = 12)
   list_site <- c(list_site, temp_list)
-  coords <- c(fire$LONG[i], fire$LAT[i])
   
+  coords <- c(fire$LONG[i], fire$LAT[i])
   list[i] <- pd_plot_slice(
   prism_archive_subset("tmean", "monthly", year = 2020),
   coords)
@@ -29,24 +31,24 @@ for (i in 1:dim(fire)[1]) {
   print(i)
 }
 
-test = do.call(rbind.data.frame, list) #create dataframe from month/tmean data
-test['name'] = list_site #add site name column to dataframe
+#converts list data into data frame
+df = do.call(rbind.data.frame, list) #create dataframe from month/tmean data
+df['name'] = list_site #add site name column to dataframe
 
-site_sub = 5 #number of sites to visualize
+site_sub = 5 #number of sites to visualize; change this # to add/remove site count
 months = 12 #number of months for which data are available in 2020
 pt_count = site_sub*months
-test_sub = test[1:pt_count,]
+df_sub = df[1:pt_count,] #subset the dataframe
 
-plot(test_sub$date, test_sub$data, pch = 16, col = factor(test_sub$name), main="Mean Monthly Temp: Ten Sites", sub="Subset of Ten Sites",
+plot(df_sub$date, df_sub$data, pch = 16, col = factor(df_sub$name), main="Mean Monthly Temp: 5 Sites",
      xlab="Month", ylab="Mean Temp (C)")
 legend(x = "topleft",                           # Add points to legend
-       legend = unique(test_sub$name),
+       legend = unique(df_sub$name),
        text.col = "black",
        lwd = 1,
-       col = unique(factor(test_sub$name)),
+       col = unique(factor(df_sub$name)),
        lty = c(0, 0),
        pch = 16,
        bty = "n",
        cex = 0.50,
        pt.cex = 1)
-
